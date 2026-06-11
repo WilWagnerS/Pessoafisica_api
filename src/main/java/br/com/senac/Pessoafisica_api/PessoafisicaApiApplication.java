8189c7db56dbe150a3e014e7f0d6b4f14892bd70
W Wagner <wilianwagnerdossantos@gmail.com>
2026-06-11 16:40:18 -0300

    Testando Pessoafisica_api

---
+++ src/main/java/br/com/senac/Pessoafisica_api/PessoafisicaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.Pessoafisica_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoafisicaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoafisicaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senac/Pessoafisica_api/PessoafisicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.Pessoafisica_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoafisicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
